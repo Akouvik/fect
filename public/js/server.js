3617aa07acff858d959453cdf042107fb17a1472
Akouvi <kouedjinl@gmail.com>
2019-02-26 22:08:58 -0700

    implemented modal on img click

--- public/js/server.js
+++ public/js/server.js
@@ -3,22 +3,6 @@ const url = "https://dog.ceo/api/breed/pomeranian/images/random/50";
 window.onload = get_pomeranian;
 const imgPerPage = 10;
 
-function render_pomeranians(data, currentPageNumber) {
-  let response = data.message;
-  let image_container = document.getElementById("image_container");
-  image_container.innerHTML = "";
-
-  let startIndex = imgPerPage * currentPageNumber; //0,10,20,30,40
-  let endIndex = imgPerPage * (currentPageNumber + 1) - 1;
-
-  for (let i = startIndex; i <= endIndex; i++) {
-    let img = document.createElement("img");
-    img.src = response[i];
-    img.alt = "Cute pomeranian";
-    image_container.appendChild(img);
-  }
-}
-
 function get_pomeranian() {
   let xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
@@ -51,4 +35,31 @@ function img_navigation(data) {
       render_pomeranians(data, i);
     });
   }
+}
+function render_pomeranians(data, currentPageNumber) {
+  let response = data.message;
+  let imageContainer = document.getElementById("imageContainer");
+  imageContainer.innerHTML = "";
+
+  let startIndex = imgPerPage * currentPageNumber; //0,10,20,30,40
+  let endIndex = imgPerPage * (currentPageNumber + 1) - 1;
+
+  for (let i = startIndex; i <= endIndex; i++) {
+    let img = document.createElement("img");
+    img.classList.add("response_img");
+    img.addEventListener("click", openModal);
+    img.src = response[i];
+    img.alt = "Cute pomeranian";
+    imageContainer.appendChild(img);
+  }
+}
+function openModal(e) {
+  document.getElementById("modImg").src = e.srcElement.currentSrc;
+  let modal = document.getElementById("modal");
+  modal.style.display = "block";
+  let close = document.getElementById("closeModal");
+  close.addEventListener("click", () => {
+    modal.style.display = "none";
+    document.getElementById("modImg").src = "";
+  });
 }
